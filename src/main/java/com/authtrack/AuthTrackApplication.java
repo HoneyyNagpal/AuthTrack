2f21604be392acd6a3a665fe8cc36031664888ad
HoneyyNagpal <honeyynagpal11@gmail.com>
2026-03-29 21:44:07 +0530

    initial project setup with spring boot and dependencies

---
+++ src/main/java/com/authtrack/AuthTrackApplication.java
@@ -0,0 +1,12 @@
+package com.authtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthTrackApplication.class, args);
+    }
+}
